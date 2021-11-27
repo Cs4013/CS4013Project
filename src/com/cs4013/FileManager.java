5093c9395adde529640ea85bf821abd6a17e8531
DavidAkin321 <daveakin321@gmail.com>
2021-11-27 04:00:49 +0000

    init

---
+++ src/com/cs4013/FileManager.java
@@ -0,0 +1,16 @@
+package com.cs4013;
+
+import java.util.ArrayList;
+
+public class FileManager {
+   private String filename = "";
+
+   public FileManager(String filename){
+       this.filename = filename;
+   }
+
+   public ArrayList<ArrayList<String>> readCsv(){
+       ArrayList<ArrayList<String>> data= new ArrayList<>();
+
+   }
+}
