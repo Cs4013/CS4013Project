df4ec8a041b99a8f62c2dfbfb3bf7d721ef02ccf
RouqX7 <farouq2169@gmail.com>
2021-11-27 04:03:47 +0000

    1st commit

---
+++ src/com/cs4013/Authentication/Booking.java
@@ -0,0 +1,15 @@
+package com.cs4013.Authentication;
+
+public class Booking {
+    private String userId;
+    private String bookingId;
+    private long checkInTime;
+    private long checkOutTime;
+    private String roomId;
+    private String hotelId;
+    private String price;
+    private String keyCard;
+    private String checkInDate;
+    private String checkOutDate;
+    private double payments;
+}
