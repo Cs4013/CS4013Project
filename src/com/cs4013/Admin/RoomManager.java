aa523ce49fbdd19b5654d0b1774a7201e4a444d2
Stephen Ajikobi <20248245@studentmail.ul.ie>
2021-11-28 04:50:59 +0000

    Admin Update 0.2

--- src/com/cs4013/Admin/RoomManager.java
+++ src/com/cs4013/Admin/RoomManager.java
@@ -4,7 +4,7 @@ import com.cs4013.Misc.StringUtils;
 import com.cs4013.Misc.TerminalLogger;
 
 public class RoomManager {
-    public int width = 50;
+    public int width = 48;
 
     public RoomManager() {
     }
